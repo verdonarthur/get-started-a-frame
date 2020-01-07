118fcc8446de0f5cfaea2a058545058a8db36d18
Arthur Verdon <verdon.arthur@gmail.com>
2020-01-07 17:03:47 +0100

    add assets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import "aframe";
+import "aframe-environment-component";
 import "aframe-super-shooter-kit";
 import "./helper";
-import { getRandomColor } from "./helper";
 
 AFRAME.registerComponent("click-to-shoot", {
   init: function() {
     document.body.addEventListener("mousedown", () => {
-      console.log("asd");
       this.el.emit("shoot");
     });
   }
@@ -16,16 +15,12 @@ AFRAME.registerComponent("shootable-box", {
   dependencies: ["material"],
   init: function() {
     let ele = this.el;
-    let color = new THREE.Color();
-    ele.setAttribute("width", 1);
-    ele.setAttribute("heigth", 1);
-    ele.setAttribute("depth", 1);
-    ele.setAttribute("geometry", { primitive: "sphere" });
 
-    ele.setAttribute("color", "#333333");
+    ele.setAttribute("gltf-model", "#targetModel");
+    ele.setAttribute("rotation", "0 180 0");
 
     ele.addEventListener("hit", evt => {
-      console.log("Touche");
+      console.log("Hit on:", ele);
     });
 
     ele.addEventListener("die", evt => {
@@ -35,49 +30,19 @@ AFRAME.registerComponent("shootable-box", {
   }
 });
 
-AFRAME.registerComponent("ground-box", {
-  init: function() {
-    this.el.setAttribute("width", 1);
-    this.el.setAttribute("heigth", 1);
-    this.el.setAttribute("depth", 1);
-
-    this.el.setAttribute("color", getRandomColor());
-  }
-});
-
-const generateGround = scene => {
-  for (let i = 1; i <= 30; i++) {
-    for (let j = 1; j <= 30; j++) {
-      let groundbox = document.createElement("a-box");
-      groundbox.setAttribute("ground-box", {});
-      groundbox.setAttribute("position", `${15 - i} -1 ${15 - j}`);
-      scene.appendChild(groundbox);
-    }
-  }
-};
-
 const generateWallToShoot = scene => {
-  let j = 0,
-    k = 0;
-
-  for (let i = 1; i <= 25; i++) {
+  for (let i = 1; i <= 5; i++) {
     let shootablebox = document.createElement("a-entity");
 
     scene.appendChild(shootablebox);
     shootablebox.setAttribute("target", { healthPoints: 1 });
     shootablebox.setAttribute("shootable-box", {});
-    shootablebox.setAttribute("position", `${-(-4 + 2 * k)} ${1 + j * 2} -10`);
-    k++;
-    if (i % 5 == 0) {
-      j++;
-      k = 0;
-    }
+    shootablebox.setAttribute("position", `${2 * (3 - i)} 0 -5`);
   }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   let mainScene = document.querySelector("#mainScene");
 
-  generateGround(mainScene);
   generateWallToShoot(mainScene);
 });
